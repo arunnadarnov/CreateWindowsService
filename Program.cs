bf9aa33ba0df5c91615d1bf34a492f17e886891d
arunnadarnov <86362043+arunnadarnov@users.noreply.github.com>
2023-06-02 16:11:27 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Configuration;
+using System.ServiceProcess;
+
+namespace CreateWindowsService
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            string exeFilePath = ConfigurationManager.AppSettings["exeFilePath"];
+
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service1(exeFilePath)
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ Service1.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.ServiceProcess;
+using System.Timers;
+
+namespace CreateWindowsService
+{
+    public partial class Service1 : ServiceBase
+    {
+        private Process myProcess;
+        private Timer checkProcessTimer;
+        private readonly string _exeFilePath;
+
+        public Service1(string exeFilePath)
+        {
+            InitializeComponent();
+            _exeFilePath = exeFilePath;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // Get the path of the service executable
+            string serviceExePath = Assembly.GetExecutingAssembly().Location;
+
+            // Get the directory that contains the service executable
+            string serviceExeDirectory = Path.GetDirectoryName(serviceExePath);
+
+            // Combine the directory path with the name of your .exe file
+            string programExePath = Path.Combine(serviceExeDirectory, _exeFilePath);
+
+            // Start a new process that runs your compiled program
+            myProcess = Process.Start(programExePath);
+
+            // Create a timer to periodically check if the process is still running
+            checkProcessTimer = new Timer(1000);
+            checkProcessTimer.Elapsed += CheckProcessTimer_Elapsed;
+            checkProcessTimer.Start();
+        }
+
+        private void CheckProcessTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            // Check if the process has exited
+            if (myProcess.HasExited)
+            {
+                // Stop the service if the process has exited
+                this.Stop();
+            }
+        }
+
+        protected override void OnStop()
+        {
+            // Stop checking if the process is still running
+            checkProcessTimer.Stop();
+
+            // Stop the process that runs your compiled program
+            if (myProcess != null && !myProcess.HasExited)
+            {
+                myProcess.Kill();
+            }
+        }
+    }
+}
